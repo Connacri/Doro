dba3e58881185529ed8e4a5891a745409c246a2c
Ramzi <connacri@gmail.com>
2026-07-01 00:32:15 +0100

    https://volte-dhyr.onrender.com

--- lib/core/bootstrap/seed_nodes.dart
+++ lib/core/bootstrap/seed_nodes.dart
@@ -1,6 +1,6 @@
 class SeedNodes {
   static const List<String> nodes = [
-    "wss://ton-serveur-signal.com", // ⚡ remplace par ton URL Render ou VPS
+    "https://volte-dhyr.onrender.com", // ⚡ remplace par ton URL Render ou VPS
   ];
 
   static List<String> getAll() => nodes;
