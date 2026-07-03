a6566d4712d2284de16a1b98ffe00b109f36edb6
Oran <95408708+Connacri@users.noreply.github.com>
2026-07-03 11:07:42 +0100

    Update genesis.dart

--- lib/core/wallet/genesis.dart
+++ lib/core/wallet/genesis.dart
@@ -31,9 +31,9 @@ class Genesis {
       "0x0000000000000000000000000000000000000000";
 
   /// Offre totale allouée au wallet fondateur au moment de sa création.
-  /// 50 000 000 DORO (18 décimales), modifiable selon les besoins.
+  /// 50 000 000 000 DORO (18 décimales), modifiable selon les besoins.
   static final BigInt maxSupply =
-      BigInt.from(50000000) * BigInt.from(10).pow(18);
+      BigInt.from(50000000000) * BigInt.from(10).pow(18);
 
   static bool isGenesisAddress(String address) => address == genesisAddress;
 
