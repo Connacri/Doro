59aa669f0222d394a83c0762ae2269895fdfa555
Oran <95408708+Connacri@users.noreply.github.com>
2026-07-06 13:45:24 +0100

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,7 @@
 import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+///
 import 'features/home/home_screen.dart';
 import 'features/wallet/wallet_provider.dart';
 import 'features/wallet/wallet_screen.dart';
